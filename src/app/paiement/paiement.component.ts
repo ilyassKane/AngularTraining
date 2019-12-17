c1baa643fb59d31f48bc7a63dc306d3d75142a6a
ilyassKane <yliasse13@yahoo.fr>
2019-12-17 18:14:02 +0100

    onsubmit

--- src/app/paiement/paiement.component.ts
+++ src/app/paiement/paiement.component.ts
@@ -1,4 +1,4 @@
-
+import { Router } from '@angular/router';
 import { Component, OnInit } from "@angular/core";
 import KRGlue from "@lyracom/embedded-form-glue";
 
@@ -10,21 +10,21 @@ import KRGlue from "@lyracom/embedded-form-glue";
 export class PaiementComponent implements OnInit {
   title = "minimal-example";
   promiseError = null;
-
+  constructor(private router: Router) { }
   ngOnInit() {
     const endpoint = "https://api.lyra.com";
-    const publicKey = "69876357:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5";
-    //const publicKey = "73239078:testpublickey_Zr3fXIKKx0mLY9YNBQEan42ano2QsdrLuyb2W54QWmUJQ";
-    
-    const formToken = "25l4g128-vT2q9YYSCEydj1g19CeyJhbW91bnQiOjUwMDAsImN1cnJlbmN5IjoiRVVSIiwibW9kZSI6IlRFU1QiLCJ2ZXJzaW9uIjozLCJzaG9wTmFtZSI6IkRlbW8gc2hvcCIsImJyYW5kUHJpb3JpdHkiOlsiQkFOQ09OVEFDVCIsIk1BRVNUUk8iXSwiY2F0ZWdvcmllcyI6eyJkZWJpdENyZWRpdENhcmRzIjp7ImFwcElkIjoiY2FyZHMiLCJwYXJhbSI6WyJBTUVYIiwiVklTQSIsIk1BRVNUUk8iLCJFLUNBUlRFQkxFVUUiLCJNQVNURVJDQVJEIiwiQkFOQ09OVEFDVCIsIlZJU0FfRUxFQ1RST04iLCJDQiJdfX0sImNhcmRzIjp7Ik1BRVNUUk8iOnsiZmllbGRzIjp7InNlY3VyaXR5Q29kZSI6eyJyZXF1aXJlZCI6ZmFsc2V9fSwiY29weUZyb20iOiJjYXJkcy5ERUZBVUxUIn0sIkFNRVgiOnsiZmllbGRzIjp7InNlY3VyaXR5Q29kZSI6eyJtYXhMZW5ndGgiOjR9fSwiY29weUZyb20iOiJjYXJkcy5ERUZBVUxUIn0sIkUtQ0FSVEVCTEVVRSI6eyJjb3B5RnJvbSI6ImNhcmRzLkRFRkFVTFQifSwiTUFTVEVSQ0FSRCI6eyJjb3B5RnJvbSI6ImNhcmRzLkRFRkFVTFQifSwiVklTQSI6eyJjb3B5RnJvbSI6ImNhcmRzLkRFRkFVTFQifSwiQkFOQ09OVEFDVCI6eyJmaWVsZHMiOnsic2VjdXJpdHlDb2RlIjp7InJlcXVpcmVkIjpmYWxzZSwiaGlkZGVuIjp0cnVlfX0sImNvcHlGcm9tIjoiY2FyZHMuREVGQVVMVCJ9LCJWSVNBX0VMRUNUUk9OIjp7ImZpZWxkcyI6eyJzZWN1cml0eUNvZGUiOnsicmVxdWlyZWQiOmZhbHNlfX0sImNvcHlGcm9tIjoiY2FyZHMuREVGQVVMVCJ9LCJERUZBVUxUIjp7ImZpZWxkcyI6eyJwYW4iOnsibWluTGVuZ3RoIjoxMCwibWF4TGVuZ3RoIjoxOSwidmFsaWRhdG9ycyI6WyJOVU1FUklDIiwiTFVITiJdLCJyZXF1aXJlZCI6dHJ1ZSwic2Vuc2l0aXZlIjp0cnVlLCJoaWRkZW4iOmZhbHNlLCJjbGVhck9uRXJyb3IiOmZhbHNlfSwiZXhwaXJ5RGF0ZSI6eyJyZXF1aXJlZCI6dHJ1ZSwic2Vuc2l0aXZlIjp0cnVlLCJoaWRkZW4iOmZhbHNlLCJjbGVhck9uRXJyb3IiOmZhbHNlfSwic2VjdXJpdHlDb2RlIjp7Im1pbkxlbmd0aCI6MywibWF4TGVuZ3RoIjozLCJ2YWxpZGF0b3JzIjpbIk5VTUVSSUMiXSwicmVxdWlyZWQiOnRydWUsInNlbnNpdGl2ZSI6dHJ1ZSwiaGlkZGVuIjpmYWxzZSwiY2xlYXJPbkVycm9yIjp0cnVlfX19LCJDQiI6eyJjb3B5RnJvbSI6ImNhcmRzLkRFRkFVTFQifX193a01";
+    //const publicKey = "69876357:testpublickey_DEMOPUBLICKEY95me92597fd28tGD4r5";
+    const publicKey = "73239078:testpublickey_Zr3fXIKKx0mLY9YNBQEan42ano2QsdrLuyb2W54QWmUJQ";
+
+    const formToken = "24c4feMB8XRXmCI7Ckye9VNw19CeyJhbW91bnQiOjk5MCwiY3VycmVuY3kiOiJFVVIiLCJtb2RlIjoiVEVTVCIsInZlcnNpb24iOjMsIm9yZGVySWQiOiJteU9yZGVySWQtOTk5OTk5Iiwic2hvcE5hbWUiOiJTbyBlYXN5IHRvIHRlc3QiLCJicmFuZFByaW9yaXR5IjpbIkNCIiwiRS1DQVJURUJMRVVFIiwiVklTQSIsIk1BU1RFUkNBUkQiLCJWSVNBX0VMRUNUUk9OIiwiTUFFU1RSTyJdLCJjYXRlZ29yaWVzIjp7ImRlYml0Q3JlZGl0Q2FyZHMiOnsiYXBwSWQiOiJjYXJkcyIsInBhcmFtIjpbIk1BRVNUUk8iLCJFLUNBUlRFQkxFVUUiLCJNQVNURVJDQVJEIiwiVklTQSIsIlZJU0FfRUxFQ1RST04iLCJDQiJdfX0sImNhcmRzIjp7Ik1BRVNUUk8iOnsiZmllbGRzIjp7InNlY3VyaXR5Q29kZSI6eyJyZXF1aXJlZCI6ZmFsc2V9fSwiY29weUZyb20iOiJjYXJkcy5ERUZBVUxUIn0sIkUtQ0FSVEVCTEVVRSI6eyJjb3B5RnJvbSI6ImNhcmRzLkRFRkFVTFQifSwiTUFTVEVSQ0FSRCI6eyJjb3B5RnJvbSI6ImNhcmRzLkRFRkFVTFQifSwiVklTQSI6eyJjb3B5RnJvbSI6ImNhcmRzLkRFRkFVTFQifSwiVklTQV9FTEVDVFJPTiI6eyJmaWVsZHMiOnsic2VjdXJpdHlDb2RlIjp7InJlcXVpcmVkIjpmYWxzZX19LCJjb3B5RnJvbSI6ImNhcmRzLkRFRkFVTFQifSwiREVGQVVMVCI6eyJmaWVsZHMiOnsicGFuIjp7Im1pbkxlbmd0aCI6MTAsIm1heExlbmd0aCI6MTksInZhbGlkYXRvcnMiOlsiTlVNRVJJQyIsIkxVSE4iXSwicmVxdWlyZWQiOnRydWUsInNlbnNpdGl2ZSI6dHJ1ZSwiaGlkZGVuIjpmYWxzZSwiY2xlYXJPbkVycm9yIjpmYWxzZX0sImV4cGlyeURhdGUiOnsicmVxdWlyZWQiOnRydWUsInNlbnNpdGl2ZSI6dHJ1ZSwiaGlkZGVuIjpmYWxzZSwiY2xlYXJPbkVycm9yIjpmYWxzZX0sInNlY3VyaXR5Q29kZSI6eyJtaW5MZW5ndGgiOjMsIm1heExlbmd0aCI6MywidmFsaWRhdG9ycyI6WyJOVU1FUklDIl0sInJlcXVpcmVkIjp0cnVlLCJzZW5zaXRpdmUiOnRydWUsImhpZGRlbiI6ZmFsc2UsImNsZWFyT25FcnJvciI6dHJ1ZX19fSwiQ0IiOnsiY29weUZyb20iOiJjYXJkcy5ERUZBVUxUIn19fQ0502";
 
     KRGlue.loadLibrary(endpoint, publicKey) /* Load the remote library */
       .then(({ KR }) =>
         KR.setFormConfig({
           /* set the minimal configuration */
           formToken: formToken,
-          "kr-language": "en-US",
-          "kr-post-url-success": "http://localhost:4200/users" /* to update initialization parameter */
+          "kr-language": "en-US"
+          //"kr-post-url-success": "http://localhost:4200/users" /* to update initialization parameter */
         })
       )
       .then(({ KR }) =>
@@ -32,11 +32,19 @@ export class PaiementComponent implements OnInit {
       ) /* add a payment form  to myPaymentForm div*/
       .then(({ KR, result }) =>
         KR.showForm(result.formId)
-      ) /* show the payment form */
+      ).then(({ KR }) => {
+        KR.onSubmit((result) => {
+          console.log("Paiement effectué ...",result);
+          if(result!=null && result.clientAnswer.orderStatus==="PAID"){
+            console.log("Paiement effectué avec succes",result);
+            this.router.navigate(['/appareils']);
+          }
+        });
+      })
       .catch(
         error => (this.promiseError = error + " (see console for more details)")
       );
   }
-  
+
 }
 
